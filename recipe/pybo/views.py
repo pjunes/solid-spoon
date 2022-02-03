52e8516a9f07f8476f0f44998c9187b96c38a61f
pjunes <qwt1597@me.com>
2022-02-03 21:18:18 +0900

    2/3_1 Separate URL

--- recipe/pybo/views.py
+++ recipe/pybo/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("안녕하세요. "
-                        "pybo.py의 views.py에서 작성된 내용입니다.")
+                        "pybo.py의 views.py에서 작성된 내용입니다."
+                        "조용재는 바보입니다.")
