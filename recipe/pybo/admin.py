68ac76f3ffbd12106ab193022ef1bc8f2ff1b04a
pjunes <qwt1597@me.com>
2022-02-04 20:40:52 +0900

    2/4_2 pybo and admin.py

--- recipe/pybo/admin.py
+++ recipe/pybo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Question
 
 # Register your models here.
+class QuetionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+admin.site.register(Question, QuetionAdmin)
